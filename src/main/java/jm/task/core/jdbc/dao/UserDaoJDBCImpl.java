0091402067a091846a572a276803377df3aeac5a
Вадим <124818150+VadimT1412@users.noreply.github.com>
2023-06-26 20:21:42 +0300

    done

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,34 +2,81 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+    private Connection connection = getConnection();
 
+    public void createUsersTable() {
+        try (Statement statement = getConnection().createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS USER (ID BIGINT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(45), LASTNAME VARCHAR(45), AGE INT)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement statement = getConnection().createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS USER");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO USER(NAME, LASTNAME, AGE) VALUES(?,?,?)")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.execute();
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (SQLException | NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM USER WHERE ID=?")) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+
     public List<User> getAllUsers() {
-        return null;
+        List<User> addAllUsers = new ArrayList<>();
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT ID, NAME, LASTNAME, AGE FROM USER");
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                addAllUsers.add(user);
+            }
+        } catch (SQLException | NullPointerException e) {
+            e.printStackTrace();
+        }
+        System.out.println(addAllUsers);
+        return addAllUsers;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement statement = getConnection().createStatement()) {
+            statement.execute("TRUNCATE TABLE USER");
+        } catch (SQLException | NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
