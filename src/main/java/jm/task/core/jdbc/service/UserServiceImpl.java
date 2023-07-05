899325570a3cd48e88954902025c8cde1453f60d
Вадим <124818150+VadimT1412@users.noreply.github.com>
2023-07-05 21:39:25 +0300

    next

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     private UserDao userDaoHibernate = new UserDaoHibernateImpl();
+
     public void createUsersTable() {
         userDaoHibernate.createUsersTable();
     }
